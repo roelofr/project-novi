457b4accbb2f1f964814b676ab7ce43f49477376
Roelof Roos <roelofhylkeroos@gmail.com>
2014-11-21 13:14:58 +0100

    Migrated from Windows Presentation Foundation to Windows Forms

---
+++ src/Project Novi/NoviLauncher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Novi
+{
+    class NoviLauncher
+    {
+        static void Main(string[] args)
+        {
+                // Open main app
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Novi());
+            
+        }
+
+    }
+}
