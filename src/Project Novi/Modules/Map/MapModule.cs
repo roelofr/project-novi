2597ff2f251690257d3f9ae54f2905abfeca3b70
Evan van Urk <EvanvUrk@gmail.com>
2014-12-10 15:17:52 +0100

    Added text for route descriptions to avatar text XML Added method to build route description from texts

--- src/Project Novi/Modules/Map/MapModule.cs
+++ src/Project Novi/Modules/Map/MapModule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Project_Novi.Text;
 
 namespace Project_Novi.Modules.Map
 {
@@ -32,6 +33,17 @@ namespace Project_Novi.Modules.Map
         public void Stop()
         {
            
+        }
+
+        public string GetRouteDescription(string classroom)
+        {
+            var numbers = classroom.ToCharArray();
+            var sb = new StringBuilder();
+
+            sb.Append(TextManager.GetText(string.Format("MapF{0}", numbers[0])));
+            sb.Append(TextManager.GetText(string.Format("MapC{0}", numbers[1])));
+
+            return sb.ToString();
         }
     }
 }
