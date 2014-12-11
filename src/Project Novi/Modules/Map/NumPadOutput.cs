543bef0125470d38860eb0030b4ff6be2acdd619
WoJoVo <woutervogelzang@gmail.com>
2014-12-11 11:39:09 +0100

    Added numpad for selecting room

---
+++ src/Project Novi/Modules/Map/NumPadOutput.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Novi.Modules.Map
+{
+    class NumPadOutput
+    {
+        public NumPad NumPad { get; set; }
+        public int ActiveDigit { get; set; }
+        public int Digits { get; set; }
+        public List<TouchButton> TouchButtons { get; set; }
+        public int Xpos { get; set; }
+        public int Ypos { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Font TextFont { get; set; }
+        public string Output { get; set; }
+        public NumPadOutput(int x, int y, int width, int height, int digits, NumPad numpad)
+        {
+            Output = "";
+            Xpos = x;
+            Ypos = y;
+            Width = width;
+            Height = height;
+            NumPad = numpad;
+            Digits = digits;
+            var textLength = 0;
+            foreach (var s in NumPad.Values)
+            {
+                if (s.Length > textLength)
+                {
+                    textLength = s.Length;
+                }
+            }
+            var textSpace = 0;
+            if ((Width / Digits) <= Height)
+            {
+                textSpace = (int)(Width / Digits);
+            }
+            else
+            {
+                textSpace = Height;
+            }
+            TextFont = new Font(NumPad.TextFont.FontFamily, (int)(0.8 * (textSpace / textLength)));
+            TouchButtons = new List<TouchButton>();
+            ActiveDigit = 0;
+            CreateNumPadOutput();
+            SetActive(ActiveDigit);
+        }
+
+        public void CreateNumPadOutput()
+        {
+            for (var digit = 0; digit < Digits; digit++)
+            {
+                TouchButtons.Add(new TouchButton(Xpos + (digit * (Width / Digits)), Ypos, (Width / Digits), Height, "_", NumPad.ButtonColor.Color, NumPad.TextColor.Color, TextFont));
+            }
+        }
+
+        public void DrawNumPadOutput(Graphics graphics)
+        {
+            foreach (var tb in TouchButtons)
+            {
+                tb.DrawButton(graphics);
+            }
+        }
+
+        public void SetActive(int digit)
+        {
+            TouchButtons.ElementAt(ActiveDigit).ActiveTimer.Reset();
+            ActiveDigit = digit;
+            TouchButtons.ElementAt(ActiveDigit).ActiveTimer.Start();
+        }
+
+        public void AddOutputDigit()
+        {
+            if (ActiveDigit != Digits - 1)
+            {
+                SetActive(ActiveDigit + 1);
+            }
+            else
+            {
+                TouchButtons.ElementAt(ActiveDigit).ActiveTimer.Reset();
+            }
+            
+        }
+
+        public void DeleteOutputDigit()
+        {
+            if (!TouchButtons.ElementAt(ActiveDigit).Value.Equals("_"))
+            {
+                TouchButtons.ElementAt(ActiveDigit).ActiveTimer.Start();
+            } 
+            else if (ActiveDigit == Digits - 1)
+            {
+                if (TouchButtons.ElementAt(ActiveDigit).Value.Equals("_"))
+                {
+                    SetActive(ActiveDigit - 1);
+                }
+                TouchButtons.ElementAt(ActiveDigit).ActiveTimer.Start();
+            }
+            else if (ActiveDigit > 0)
+            {
+                SetActive(ActiveDigit - 1);
+            }
+        }
+
+        public void BuildOutput()
+        {
+            Output = "";
+            foreach (var tb in TouchButtons)
+            {
+                if (!tb.Value.Equals("_"))
+                {
+                  Output += tb.Value;  
+                }
+            }
+        }
+
+        public void ClearOutput()
+        {
+            ActiveDigit = 0;
+            foreach (var tb in TouchButtons)
+            {
+                tb.Value = "_";
+            }
+            AddOutputDigit();
+        }
+
+    }
+}
---
+++ src/Project Novi/Modules/Map/TouchButton.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Project_Novi.Modules.Map
+{
+    class TouchButton
+    {
+        public int Xpos { get; set; }
+        public int Ypos { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Value { get; set; }
+        public SolidBrush ButtonColor { get; set; }
+        public SolidBrush TextColor { get; set; }
+        public Font TextFont { get; set; }
+        public bool Enabled { get; set; }
+        private Stopwatch TouchTimer;
+        public Stopwatch ActiveTimer;
+
+        private readonly StringFormat _formatText = new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
+        public TouchButton(int x, int y, int width, int height, string value, Color buttoncolor, Color textcolor, Font font)
+        {
+            Xpos = x;
+            Ypos = y;
+            Width = width;
+            Height = height;
+            Value = value;
+            ButtonColor = new SolidBrush(Color.FromArgb(200, buttoncolor));
+            TextColor = new SolidBrush(Color.FromArgb(200, textcolor));
+            TextFont = font;
+            Enabled = true;
+            ActiveTimer = new Stopwatch();
+            TouchTimer = new Stopwatch();
+        }
+
+        public bool IsClicked(Point p)
+        {
+            if (p.X >= Xpos && p.X <= Xpos + Width && p.Y >= Ypos && p.Y <= Ypos + Height && Enabled)
+            {
+                TouchTimer.Start();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public void DrawButton(Graphics g)
+        {
+            if (TouchTimer.ElapsedMilliseconds < 250 && TouchTimer.IsRunning)
+            {
+                ButtonColor = new SolidBrush(Color.FromArgb(255, ButtonColor.Color));
+                TextColor = new SolidBrush(Color.FromArgb(255, TextColor.Color));
+            }
+            else if (Enabled)
+            {
+                ButtonColor = new SolidBrush(Color.FromArgb(200, ButtonColor.Color));
+                TextColor = new SolidBrush(Color.FromArgb(200, TextColor.Color));
+                TouchTimer.Reset();
+            }
+            else
+            {
+                ButtonColor = new SolidBrush(Color.FromArgb(100, ButtonColor.Color));
+                TextColor = new SolidBrush(Color.FromArgb(100, TextColor.Color));
+            }
+            var buttonRect = new Rectangle(Xpos, Ypos, Width, Height);
+            g.FillRectangle(ButtonColor, buttonRect);
+            if (ActiveTimer.IsRunning && Math.Floor((decimal) (ActiveTimer.ElapsedMilliseconds/500))%2 == 0)
+            {
+                g.DrawString(" ", TextFont, TextColor, buttonRect, _formatText);
+            }
+            else
+            {
+                g.DrawString(Value, TextFont, TextColor, buttonRect, _formatText);
+            }
+            
+        }
+    }
+}
