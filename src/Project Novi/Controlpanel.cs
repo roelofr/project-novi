4a0860dc9e230799ba73f842f52a377d035c67a6
Janjaap Ree <janjaapree@gmail.com>
2014-12-16 14:01:15 +0100

    Added save twitter settings to xml

--- src/Project Novi/Controlpanel.cs
+++ src/Project Novi/Controlpanel.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.VisualBasic;
+using System.Xml;
 
 namespace Project_Novi
 {
@@ -17,8 +10,7 @@ namespace Project_Novi
         public Controlpanel()
         {
             InitializeComponent();
-           
-
+            GetUsernamesTwitter();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -31,12 +23,55 @@ namespace Project_Novi
             switch (e.KeyCode)
             {
                 case Keys.Enter:
+                    SaveUsernames();
                     Close();
                     break;
                 case Keys.Escape:
                     Close();
                     break;
             }
+        }
+
+        public void GetUsernamesTwitter()
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load("TwitterSettings.xml");
+
+            XmlNode nodeUsername1 = xmlDoc.DocumentElement.SelectSingleNode(String.Format("/Strings/username1/String"));
+            XmlNode nodeUsername2 = xmlDoc.DocumentElement.SelectSingleNode(String.Format("/Strings/username2/String"));
+            XmlNode nodeUsername3 = xmlDoc.DocumentElement.SelectSingleNode(String.Format("/Strings/username3/String"));
+
+            textboxUsernameTwitter1.Text = nodeUsername1.InnerText;
+            textboxUsernameTwitter2.Text = nodeUsername2.InnerText;
+            textboxUsernameTwitter3.Text = nodeUsername3.InnerText;
+        }
+
+        public static string GetUsernameTwitter(string usernameNumber)
+        {
+            //username1
+            //username2
+            //username3
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load("TwitterSettings.xml");
+
+            var nodeList = xmlDoc.DocumentElement.SelectNodes(String.Format("/Strings/{0}/String", usernameNumber));
+            
+            return nodeList[0].InnerText;  
+        }
+
+        public void SaveUsernames()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("TwitterSettings.xml");
+            XmlNode nodeUsername1 = doc.SelectSingleNode(String.Format("/Strings/username1/String"));
+            XmlNode nodeUsername2 = doc.SelectSingleNode(String.Format("/Strings/username2/String"));
+            XmlNode nodeUsername3 = doc.SelectSingleNode(String.Format("/Strings/username3/String"));
+
+            nodeUsername1.InnerText = textboxUsernameTwitter1.Text;
+            nodeUsername2.InnerText = textboxUsernameTwitter2.Text;
+            nodeUsername3.InnerText = textboxUsernameTwitter3.Text;
+
+            doc.Save("TwitterSettings.xml");
         }
     }
 }
