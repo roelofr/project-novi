28fb7f25de025ebc0a160d25e409095fed98333d
Arjan van der Weide <arjanvdweide@live.nl>
2014-12-17 11:19:45 +0100

    Fixed issue where text on T0 was not right. It's all right now

--- src/Project Novi/Modules/Map/MapModule.cs
+++ src/Project Novi/Modules/Map/MapModule.cs
@@ -48,8 +48,29 @@ namespace Project_Novi.Modules.Map
 
             sb.Append(TextManager.GetText(string.Format("MapF{0}", classroom[0])));
 
+            if (classroom[0] == '0')
+            {
+                switch (classroom[1])
+                {
+                    case '0':
+                    case '1':
+                    case '2':
+                        sb.Append(TextManager.GetText("MapBeganeGrondLinks"));
+                        break;
+                    case '3':
+                        //If the classroom is in the middle of the building
+                        sb.Append(TextManager.GetText("MapBeganeGrondMidden"));
+                        break;
+                    case '4':
+                    case '5':
+                    case '6':
+                        //If the classroom is in the left half of the building
+                        sb.Append(TextManager.GetText("MapBeganeGrondRechts"));
+                        break;
+                }
+            }
             //If the floor is ground floor, first floor or second floor (and we want to take the stairs)
-            if (classroom[0] == '0' || classroom[0] == '1' || classroom[0] == '2')
+            else if (classroom[0] == '0' || classroom[0] == '1' || classroom[0] == '2')
             {
                 switch (classroom[1])
                 {
