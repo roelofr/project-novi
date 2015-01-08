edea8d4ab7cb22a4d122658d1c22884ef7386174
Roelof Roos <roelofhylkeroos@gmail.com>
2015-01-08 16:13:27 +0100

    Added NS module

---
+++ src/Vertrektijden/NSModule.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_Novi.Api;
+
+namespace Vertrektijden
+{
+    public class NSModule : IModule
+    {
+        public string Name
+        {
+            get { return "NS"; }
+        }
+
+        public Bitmap Icon
+        {
+            get { return Properties.Resources.train_icon; }
+        }
+
+        public string DisplayName
+        {
+            get { return "Actuele vertrektijd"; }
+        }
+
+        public bool Rotatable
+        {
+            get { return true; }
+        }
+
+        public void Initialize(IController controller)
+        {
+            // Not doing anything on init
+        }
+
+        public void Start()
+        {
+            // Not doing anything on start
+        }
+
+        public void Stop()
+        {
+            // Not doing anything on stop
+        }
+    }
+}
---
+++ src/Vertrektijden/NSView.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project_Novi.Api;
+using Project_Novi.Background;
+
+namespace Vertrektijden
+{
+    class NSView : IView
+    {
+        private IController _controller;
+        private NSModule _module;
+
+        public Type ModuleType
+        {
+            get { return typeof(NSModule); }
+        }
+
+        public IBackgroundView BackgroundView { get; private set; }
+
+        public void Initialize(IController controller)
+        {
+            BackgroundView = new SubBackground(controller, true);
+            _controller = controller;
+        }
+
+        public void Attach(IModule module)
+        {
+            var newsModule = module as NSModule;
+            if (newsModule != null)
+            {
+                _module = newsModule;
+            }
+            else
+                throw new ArgumentException("A NewsView can only render the interface for a NewsModule");
+
+        }
+
+        public void Detach()
+        {
+            _module = null;
+        }
+
+        public void Render(Graphics graphics, Rectangle rectangle)
+        {
+            var font = new Font("Open Sans", 18, FontStyle.Bold);
+            var align = new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
+            
+            // Not drawing anything special yet, just a "WIP" message
+            graphics.DrawString("Dit systeem is nog niet beschikbaar", font, Brushes.LightGray, rectangle, align);
+        }
+    }
+}
