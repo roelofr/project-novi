a7c453ceedcb660adab2f58808d842ed074e21cf
Joram Schrijver <i@joram.io>
2015-01-09 11:30:13 +0100

    Clean up Twitter module and add overview

--- src/Twitter/Tweet.cs
+++ src/Twitter/Tweet.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Twitter
 {
@@ -11,12 +8,16 @@ namespace Twitter
         public string ScreenName { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public string Text { get; private set; }
+        public Image Image { get; private set; }
+        public string Source { get; private set; }
 
-        public Tweet(string screenname, DateTime createdAt, string text)
+        public Tweet(string screenname, DateTime createdAt, string text, Image image, string source)
         {
-            this.ScreenName = screenname;
-            this.CreatedAt = createdAt;
-            this.Text = text;
+            ScreenName = screenname;
+            CreatedAt = createdAt;
+            Text = text;
+            Image = image;
+            Source = source;
         }
     }
 }
